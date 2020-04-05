c5046cb571368b0278e4003e38716226a2723d5a
Emily Thompson <ekthompson59@gmail.com>
2020-04-04 23:34:50 -0700

    Initial commit

--- WishList v1/HomeVC.swift
+++ WishList v1/HomeVC.swift
@@ -5,16 +5,20 @@
 //  Created by Emily Thompson on 4/4/20.
 //  Copyright © 2020 Emily Thompson. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func upcomingButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "upcomingButtonPressed", sender: self)
+    }
+    
 }
+    
 
---
+++ WishList v1/UpcomingVC.swift
@@ -0,0 +1,20 @@
+//
+//  UpcomingVC.swift
+//  WishList v1
+//
+//  Created by Emily Thompson on 4/4/20.
+//  Copyright © 2020 Emily Thompson. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+}
